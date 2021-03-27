e8e0e3f87fc2cb4134af3eda9480f9bcbd919ceb
Mashkoor Hasan <mashkoorhasan147@gmail.com>
2021-03-27 10:23:57 +0530

    Add Activity for SignIn and Sign Up

---
+++ app/src/main/java/com/realtime/car/sharing/Activities/SignUp.java
@@ -0,0 +1,16 @@
+package com.realtime.car.sharing.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.realtime.car.sharing.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
